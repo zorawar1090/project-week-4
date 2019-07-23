fe07fa23d196a447f15ada377bca0700391a9cb2
halynaburdiian <verdamtion@gmail.com>
2019-07-23 10:21:24 +0200

    add game-1-imge-component to the game-1-container

--- src/components/Game1Container.js
+++ src/components/Game1Container.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Game1QuestionImageComponent from './Game1QuestionImageComponent'
 
 export default class Game1Container extends React.Component{
   render(){
     return(
       <div>
         <Link to={`/dog-breeds`}>List all the dog breeds</Link>
+        <Game1QuestionImageComponent/>
       </div>
     )
   }

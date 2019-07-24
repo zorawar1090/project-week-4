88acb1b1c3d78374a69eb4beecb9b08873dab290
halynaburdiian <verdamtion@gmail.com>
2019-07-24 11:38:06 +0200

    connecting redux

--- src/components/Game2Container.js
+++ src/components/Game2Container.js
@@ -1,8 +1,28 @@
 import React,{Component} from 'react'
 import Game2CardItem from './Game2CardItem'
+import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import {incrementCorrect, incrementIncorrect} from '../actions/game-2'
 
-export default class Game2Container extends Component{
+ class Game2Container extends Component{
  render(){
-     return <Game2CardItem/>
+     console.log('CORRECT:',this.props.correctAnswers )
+     return <Game2CardItem 
+     correctAnswers={this.props.correctAnswers} 
+     incorrectAnswers={this.props.incorrectAnswers}
+     incrementCorrect={this.props.incrementCorrect}
+     incrementIncorrect={this.props.incrementIncorrect}
+     />
  }
 }
+const mapStateToProps = state => ({
+    correctAnswers: state.correctAnswers,
+    incorrectAnswers: state.incorrectAnswers,
+  });
+  
+  const mapDispatchToProps = dispatch => bindActionCreators({ incrementCorrect, incrementIncorrect }, dispatch);
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(Game2Container);
---
+++ src/reducers/game-2.js
@@ -0,0 +1,20 @@
+import {INCREMENT_CORRECT} from '../actions/game-2'
+import {INCREMENT_INCORRECT} from '../actions/game-2'
+
+export default (state = {correctAnswers:0,incorrectAnswers:0}, action = {}) => {
+    switch(action.type){
+    case INCREMENT_CORRECT:
+        return {
+            ...state,
+            correctAnswers: state.correctAnswers + 1,
+            totalAnswers: state.totalAnswers + 1,
+        }
+    case INCREMENT_INCORRECT:
+        return {
+            ...state,
+            incorrectAnswers: state.incorrectAnswers + 1,
+            totalAnswers: state.totalAnswers + 1,
+        }
+    default: return state
+    }
+}

7c46ffbaeac95fcd0c69d5ee82fee3639db78330
halynaburdiian <verdamtion@gmail.com>
2019-07-25 15:41:34 +0200

    Removing unusable code

--- src/components/Game2CardItem.js
+++ src/components/Game2CardItem.js
@@ -15,7 +15,6 @@ export default class Game2CardItem extends Component{
             .get(`https://dog.ceo/api/breeds/image/random/3`)
             .then(response => {
                 const correctIndex = (Math.floor(Math.random()*3));
-                //console.log('my current index is:', correctIndex)
                 this.setState({
                     image: response.body.message,
                     breed: response.body.message[correctIndex].split('/')[4],
@@ -28,16 +27,11 @@ export default class Game2CardItem extends Component{
 
     handleClick=(event)=>{
         const index = event.target.getAttribute('data-index')
-        //console.log('my index is:',index)
-        //console.log('current:', this.state.correctIndex)
         if (parseInt(index) === this.state.correctIndex){
-            this.setState({ feedback: true })
-            this.props.userHasAnswered(true)    
+            this.setState({ feedback: true })   
             setTimeout(() => {
                 this.setState({ feedback: false });
                 this.getData();
-                // this.props.incrementCorrect();
-                // this.props.updateSuccess();
                 this.props.updateCorrectThunk();
             }, 1000);
         }
@@ -46,15 +40,12 @@ export default class Game2CardItem extends Component{
             setTimeout(() => {
                 this.setState({ feedback: false });
                 this.getData();
-                // this.props.incrementIncorrect();
-                // this.props.updateSuccess();
                 this.props.updateIncorrectThunk()
             }, 2000);
         }
     }
 
     render(){
-        //console.log('Breed:', this.state.breed)
         return <div>
             <h2>Choose the image of {this.state.breed}</h2>
             {this.state.image.map((image,index) => {

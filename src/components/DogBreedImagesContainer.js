e2a2a6f4837dc7bd3de7ea0ee50d059b2603922e
halynaburdiian <verdamtion@gmail.com>
2019-07-26 13:45:44 +0200

    Show 10 images of every breed

--- src/components/DogBreedImagesContainer.js
+++ src/components/DogBreedImagesContainer.js
@@ -20,6 +20,9 @@ export default class DogBreedImagesContainer extends React.Component{
     }
 
     render(){
+        if (this.state.images>10){
+            return {images :null}
+        }
         return <DogBreedImages images={ this.state.images } breed={this.props.match.params.breed} />
     }
 }
